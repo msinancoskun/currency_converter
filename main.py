1411baefd08b79ac580f2b063dfe3bc74d198a02
Sinan Coskun <msinancoskun@yandex.com>
2021-08-29 23:30:49 +0300

    "third committed"

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import requests
 
 
 API_KEY = 'ad569808e32a64605e44daea'
-
-FROM_CURRENCY = input("from...").upper()
-TO_CURRENCY = input("to...").upper()
+currency_dict = {}
 
 
 def create_json_currency():
@@ -14,24 +12,24 @@ def create_json_currency():
 
 
 def convert(from_cur, to_cur, amount: int):
+    url = 'https://v6.exchangerate-api.com/v6/{0}/latest/{1}'.format(API_KEY, from_cur)
+    source = requests.get(url).json()
+    conversion_rates = source['conversion_rates']
+    for currency, rate in conversion_rates.items():
+        currency_dict[currency] = rate
     if from_cur in currency_dict.keys() and to_cur in currency_dict.keys():
         conversion = float(currency_dict[to_cur]) * amount
-        return "1 USD = {} {}".format(conversion, to_cur)
-    else:
-        return "Currencies {}-{} not found".format(from_cur, to_cur), convert(FROM_CURRENCY, TO_CURRENCY, amount=int(
-            input("What is the amount of currency?")))
+        print("{} {} = {:.2f} {}".format(amount, from_cur, conversion, to_cur))
 
 
 if __name__ == '__main__':
-    URL = 'https://v6.exchangerate-api.com/v6/{0}/latest/{1}'.format(API_KEY, FROM_CURRENCY)
-    source = requests.get(URL).json()
-    currency_dict = {}
-    conversion_rates = source['conversion_rates']
+    control = True
+    while control:
+        try:
+            convert(input("from...").upper(), input("to...").upper(), int(input("What is the amount of currency?\n")))
+            quit_program = input("Q to quit! Press ENTER to continue...").upper()
+            if quit_program == 'Q':
+                control = False
 
-    for currency, rate in conversion_rates.items():
-        currency_dict[currency] = rate
-
-    while True:
-        print(convert(FROM_CURRENCY, TO_CURRENCY, amount=int(input("What is the amount of currency?"))))
-        if FROM_CURRENCY == 'quit' or TO_CURRENCY == 'quit':
-            break
+        except KeyError:
+            print("Currency not found! Try again...")
